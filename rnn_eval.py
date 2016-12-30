8b697c2ee587230cd66ddb160dbb0311bc3077f2
Matt Harvey <harveym@gmail.com>
2016-12-30 10:31:13 -0800

    Tweaks

--- rnn_eval.py
+++ rnn_eval.py
@@ -2,7 +2,7 @@
 Run a holdout set of data through our trained RNN. Requires we first
 run train_rnn.py and save the weights.
 """
-from rnn_utils import get_network, get_network_deep, get_network_wider, get_data
+from rnn_utils import get_network, get_network_deep, get_network_wide, get_data
 import tflearn
 
 def main(filename, frames, batch_size, num_classes, input_length):
@@ -14,7 +14,7 @@ def main(filename, frames, batch_size, num_classes, input_length):
     num_classes = len(y_train[0])
 
     # Get our network.
-    net = get_network_wider(frames, input_length, num_classes)
+    net = get_network_wide(frames, input_length, num_classes)
 
     # Get our model.
     model = tflearn.DNN(net, tensorboard_verbose=0)
@@ -24,10 +24,10 @@ def main(filename, frames, batch_size, num_classes, input_length):
     print(model.evaluate(X_train, y_train))
 
 if __name__ == '__main__':
-    # filename = 'data/predicted-frames-1.pkl'
-    # input_length = 2
-    filename = 'data/cnn-features-frames-2.pkl'
-    input_length = 2048
+    filename = 'data/predicted-frames-2.pkl'
+    input_length = 2
+    # filename = 'data/cnn-features-frames-2.pkl'
+    # input_length = 2048
     frames = 40
     batch_size = 32
     num_classes = 2
--- rnn_train.py
+++ rnn_train.py
@@ -2,7 +2,7 @@
 Given a saved output of predictions or pooled features from our CNN,
 train an RNN (LSTM) to examine temporal dependencies.
 """
-from rnn_utils import get_network, get_network_deep, get_network_wider, get_data
+from rnn_utils import get_network, get_network_deep, get_network_wide, get_data
 import tflearn
 
 def main(filename, frames, batch_size, num_classes, input_length):
@@ -14,7 +14,7 @@ def main(filename, frames, batch_size, num_classes, input_length):
     num_classes = len(y_train[0])
 
     # Get our network.
-    net = get_network_wider(frames, input_length, num_classes)
+    net = get_network_wide(frames, input_length, num_classes)
 
     # Train the model.
     model = tflearn.DNN(net, tensorboard_verbose=0)
@@ -26,10 +26,10 @@ def main(filename, frames, batch_size, num_classes, input_length):
     model.save('checkpoints/rnn.tflearn')
 
 if __name__ == '__main__':
-    filename = 'data/cnn-features-frames-1.pkl'
-    input_length = 2048
-    # filename = 'data/predicted-frames-1.pkl'
-    # input_length = 2
+    # filename = 'data/cnn-features-frames-1.pkl'
+    # input_length = 2048
+    filename = 'data/predicted-frames-1.pkl'
+    input_length = 2
     frames = 40
     batch_size = 32
     num_classes = 2
